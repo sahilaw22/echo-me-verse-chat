d670c6042380a7c1d91e8b9c85b96ca77f5b585c
Your Name <your.email@example.com>
2025-04-27 19:18:51 +0530

    Initial commit for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import { configDefaults } from "vitest/config";
 
 export default defineConfig({
+  base: '/ProjectCrazeX/',
   plugins: [react()],
   resolve: {
     alias: {
